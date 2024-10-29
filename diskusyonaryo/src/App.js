51c0c1391a7699ce567e4a98b790b50c5417d9a5
AlbinCayanan <aacayanan@up.edu.ph>
2024-10-30 00:15:42 +0800

    about page with react

--- diskusyonaryo/src/App.js
+++ diskusyonaryo/src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header'; // Adjust the path if needed
 import Homepage from './pages/Homepage/Homepage';
 import Discussion from './pages/Discussion/Discussion';
+import About from './pages/About/About';
 import WordDiscussionExpanded from './components/WordDiscussion/WordDiscussionExpanded';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return (
@@ -13,6 +15,7 @@ const App = () => {
                 {/* Route for the Discussion page */}
                 <Route path="/" element={<Homepage />} />
                 <Route path="/discussion" element={<Discussion />} />
+                <Route path="/about" element={<About />} />
                 
                 {/* Route for the expanded discussion page */}
                 <Route path="/discussion/:id" element={<WordDiscussionExpanded />} />
